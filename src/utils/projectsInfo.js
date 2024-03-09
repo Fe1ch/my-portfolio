ffe931595f3dd272de6c6a4923e246c25f1537bb
Fe1ch <linkin-park_94@mail.ru>
2024-03-09 09:19:42 +0300

    add project

--- src/utils/projectsInfo.js
+++ src/utils/projectsInfo.js
@@ -1,6 +1,7 @@
 import HowToLearnImg from "../images/projects/How to Learn.png";
 import RussianTravelImg from "../images/projects/Russian Travel.png";
 import MestoImg from "../images/projects/Mesto.png";
+import RickAndMortyImg from "../images/projects/RickAndMorty.png";
 
 import ReactIcon from "../images/skills/react.svg";
 import ExpressIcon from "../images/skills/express.svg";
@@ -8,10 +9,26 @@ import HtmlIcon from "../images/skills/html5.svg";
 import CssIcon from "../images/skills/css3.svg";
 import GithubIcon from "../images/skills/github-90.png";
 import NodeJsIcon from "../images/skills/nodejs.svg";
+import ReduxIcon from "../images/skills/Redux.svg";
 
 export const PROJECTS_INFO = [
   {
     id: "1",
+    title: "Rick And Morty",
+    description: "Можно сохранять героев из Rick And Morty",
+    websiteUrl: "https://fe1ch.github.io/the-rick-and-morty/",
+    githubUrl: "https://github.com/Fe1ch/the-rick-and-morty",
+    githubIcon: GithubIcon,
+    image: RickAndMortyImg,
+    techIcons: [
+      { name: "HTML", icon: HtmlIcon },
+      { name: "CSS", icon: CssIcon },
+      { name: "React", icon: ReactIcon },
+      { name: "Redux", icon: ReduxIcon },
+    ],
+  },
+  {
+    id: "2",
     title: "Mesto",
     description:
       "Регистрируйтесь, делитесь фото в общей ленте вместе с друзьями. Ставьте лайки.Мини Instagram.",
@@ -23,12 +40,12 @@ export const PROJECTS_INFO = [
       { name: "HTML", icon: HtmlIcon },
       { name: "CSS", icon: CssIcon },
       { name: "React", icon: ReactIcon },
-      { name: "Sass", icon: NodeJsIcon },
+      { name: "NodeJS", icon: NodeJsIcon },
       { name: "Express", icon: ExpressIcon },
     ],
   },
   {
-    id: "2",
+    id: "3",
     title: "Russian Travel",
     description: "Адаптивный сайт. О путешествии по нашей необъятной стране.",
     websiteUrl: "https://fe1ch.github.io/russian-travel/",
@@ -41,7 +58,7 @@ export const PROJECTS_INFO = [
     ],
   },
   {
-    id: "3",
+    id: "4",
     title: "How to Learn",
     description: "Статичный сайт. Подсказки по эффективному обучению.",
     websiteUrl: "https://fe1ch.github.io/how-to-learn/",
